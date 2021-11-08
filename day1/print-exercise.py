ae83c6d7a073b0542241897fd343689db9938455
James Torres <james.torres01@gmail.com>
2021-11-08 12:12:18 -0500

    day one

---
+++ day1/print-exercise.py
@@ -0,0 +1,7 @@
+# printing exercise 1
+print("Day 1 - Python Print Function")
+
+print("The function is delcared like this:")
+
+print("print('what to print')")
+# double quotes used for the string,single quotes used for using quotation marks in the string
---
+++ day1/print.py
@@ -0,0 +1,16 @@
+print("Hello " + input("What is your name?"))
+
+# print function to print to the console | print()
+
+# when printing strings you can use single or double quotes
+
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+# when starting to code, we think we need to do the above to print the same thing on individual lines.
+
+print("Hello World!\nHello World!")
+# to print on a new line use \n this will print the second Hello World on the new line.
+
+print("Hello " + "James")
+# the + is used for string concatenation, the combining of 2 strings
