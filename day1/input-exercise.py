fbc17132e318fb9199e442ebfd7c7c51e02c8746
James Torres <james.torres01@gmail.com>
2021-11-08 20:05:50 -0500

    data

---
+++ day1/input-exercise.py
@@ -0,0 +1,5 @@
+# write a program that prints the number of characters in a users name
+
+print(len (input("What is your name? ") ) )
+
+# len function : len() will tell you the length of whats in the function.
--- day1/print.py
+++ day1/print.py
@@ -1,16 +1,22 @@
-print("Hello " + input("What is your name?"))
-
 # print function to print to the console | print()
 
 # when printing strings you can use single or double quotes
+print("Hello World!")
 
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
 # when starting to code, we think we need to do the above to print the same thing on individual lines.
 
-print("Hello World!\nHello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+
 # to print on a new line use \n this will print the second Hello World on the new line.
 
-print("Hello " + "James")
+print("Hello World!\nHello World!")
+
 # the + is used for string concatenation, the combining of 2 strings
+
+print("Hello " + "James")
+
+# the input function allows the user to enter data in the console, when the code is run the cursor will be waiting for the users response
+
+print("Hello " + input("What is your name?"))
